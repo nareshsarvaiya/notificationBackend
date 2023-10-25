179ce3c7edd9949ab35012c989b1d61228c5c9b5
naresh sarvaiya <nareshsarvaiya28@gmail.com>
2023-10-25 21:28:15 +0530

    revert code

--- src/main/java/com/notification/notification/controller/NotificationController.java
+++ src/main/java/com/notification/notification/controller/NotificationController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +24,6 @@ public class NotificationController {
 
 	@PostMapping("/submit")
 	public List<Notification> saveNotification(@RequestBody List<Notification> notifications) {
-		System.out.println("test");
 		return notificationService.saveNotification(notifications);
 	}
 
